dd79267bc7f7f7175affaecca5324911a7722c17
sonsurim <surim014@naver.com>
2024-01-10 01:52:42 +0900

    fetching data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,7 +34,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
+  const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
