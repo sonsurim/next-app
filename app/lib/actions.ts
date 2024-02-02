51569eb5fcb51a9c355f16dc9da98d8913122f3e
sonsurim <surim014@naver.com>
2024-02-02 17:36:42 +0900

    Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
 
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { AuthError } from 'next-auth'
 import { z } from 'zod'
+import { signIn } from '@/auth'
 
 const FormSchema = z.object({
   id: z.string(),
@@ -96,5 +99,23 @@ export const deleteInvoice = async (id: string) => {
     return { message: 'Deleted Invoice.' }
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
+}
+
+export const authenticate = async (prevState: string | undefined, formData: FormData) => {
+  console.log('auth')
+
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.'
+        default:
+          return 'Something went wrong.'
+      }
+    }
+    throw error
   }
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import { ArrowRightIcon } from '@heroicons/react/20/solid'
 import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline'
+import { useFormState, useFormStatus } from 'react-dom'
 import { Button } from './button'
+import { authenticate } from '@/app/lib/actions'
 import { lusitana } from '@/app/ui/fonts'
 
 export default function LoginForm() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined)
+
   return (
-    <form className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>Please log in to continue.</h1>
         <div className="w-full">
@@ -44,15 +50,24 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1">{/* Add form errors here */}</div>
+        <div aria-atomic="true" aria-live="polite" className="flex h-8 items-end space-x-1">
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
       </div>
     </form>
   )
 }
 
 const LoginButton = () => {
+  const { pending } = useFormStatus()
+
   return (
-    <Button className="mt-4 w-full">
+    <Button aria-disabled={pending} className="mt-4 w-full">
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   )
