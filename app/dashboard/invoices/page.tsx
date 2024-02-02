eebee3588721f68d76b9ad521069b706436ee41c
sonsurim <surim014@naver.com>
2024-02-02 17:43:44 +0900

    Adding Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,12 +1,16 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import type { Metadata } from 'next'
+import { Suspense } from 'react'
+import { fetchInvoicesPages } from '@/app/lib/data'
+import { lusitana } from '@/app/ui/fonts'
+import { CreateInvoice } from '@/app/ui/invoices/buttons'
+import Pagination from '@/app/ui/invoices/pagination'
+import Table from '@/app/ui/invoices/table'
+import Search from '@/app/ui/search'
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 
+export const metadata: Metadata = {
+  title: 'Invoices'
+}
 
 type PageProps = {
   searchParams?: {
@@ -29,12 +33,12 @@ export default async function Page({ searchParams }: PageProps) {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table currentPage={currentPage} query={query} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
     </div>
-  );
+  )
 }
